916c8ab116ccbe9672760ba859f407af450fb25d
mehdi-sajedi <mehdiisajedi@gmail.com>
2021-11-20 10:41:58 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import MoviesGrid from './components/MoviesGrid';
+import './app.scss';
+
+const themovieDB = `https://api.themoviedb.org/3/discover/movie?vote_count.gte=2500&vote_average.gte=7.8&sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = () => {
+      fetch(themovieDB)
+        .then((data) => data.json())
+        .then((data) => {
+          setMovies(data.results);
+          console.log(data.results);
+        });
+    };
+
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <MoviesGrid movies={movies} setMovies={setMovies} />
+    </main>
   );
 }
 
