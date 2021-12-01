1493bd2035856fdd96b31d43c494207001ebfafe
mehdi-sajedi <mehdiisajedi@gmail.com>
2021-11-30 17:18:28 -0800

    Search partially working for people, tv, and movie results

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,10 @@ import NavStyles from './Nav.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { HiOutlineSearch } from 'react-icons/hi';
 
+// hit the multi route
+// if the first result is a person, show the persons work like we are doing in the main branch
+// if the first result is NOT a person, show a combination of tv shows and movies based on the search text
+
 const Nav = () => {
   const { dispatch } = useContext(AppContext);
   const navigate = useNavigate();
@@ -15,27 +19,56 @@ const Nav = () => {
 
     const person = `https://api.themoviedb.org/3/search/person?api_key=${process.env.REACT_APP_API_KEY}&query=${e.target[0].value}`;
 
+    const multi = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${e.target[0].value}`;
+
     const getResults = () => {
       let personResults = [];
 
-      fetch(person)
+      fetch(multi)
         .then((data) => data.json())
         .then(({ results }) => {
           console.log(results);
           if (results.length === 0) return;
-          const personID = results[0].id;
-          fetch(
-            `https://api.themoviedb.org/3/person/${personID}/combined_credits?api_key=${process.env.REACT_APP_API_KEY}`
-          )
-            .then((data) => data.json())
-            .then((stuff) => {
-              personResults = stuff.cast;
-              console.log(personResults);
-              dispatch({ type: 'SET-RESULTS', payload: personResults });
-              stuff.cast.forEach((entry) => {
-                console.log(entry.media_type);
+
+          const finder = results.find((entry) => {
+            return (
+              entry.name?.toLowerCase() === `${e.target[0].value.toLowerCase()}`
+            );
+          });
+
+          console.log(finder && 'it is true');
+
+          if (results[0].media_type === 'person' || finder) {
+            // PERSON
+            let personID;
+
+            if (finder) {
+              personID = finder.id;
+            } else {
+              personID = results[0].id;
+            }
+
+            console.log(personID);
+
+            fetch(
+              `https://api.themoviedb.org/3/person/${personID}/combined_credits?api_key=${process.env.REACT_APP_API_KEY}`
+            )
+              .then((data) => data.json())
+              .then((stuff) => {
+                personResults = stuff.cast;
+                console.log(personResults);
+                dispatch({ type: 'SET-RESULTS', payload: personResults });
+                stuff.cast.forEach((entry) => {
+                  console.log(entry.media_type);
+                });
               });
-            });
+          } else if (results[0].media_type !== 'person') {
+            // TV & MOVIE
+            const onlyTvAndMovie = results.filter(
+              (entry) => entry.media_type !== 'person'
+            );
+            dispatch({ type: 'SET-RESULTS', payload: onlyTvAndMovie });
+          }
         });
     };
 
