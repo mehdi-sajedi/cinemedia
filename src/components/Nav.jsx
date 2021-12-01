ebd33659cdd238719fb6ff8b2ad4b288ef909b77
mehdi-sajedi <mehdiisajedi@gmail.com>
2021-11-30 19:00:57 -0800

    Refactored form submit code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,76 +4,49 @@ import NavStyles from './Nav.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { HiOutlineSearch } from 'react-icons/hi';
 
-// hit the multi route
-// if the first result is a person, show the persons work like we are doing in the main branch
-// if the first result is NOT a person, show a combination of tv shows and movies based on the search text
-
 const Nav = () => {
   const { dispatch } = useContext(AppContext);
   const navigate = useNavigate();
   const searchRef = useRef();
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
     const searchText = searchRef.current.value;
     if (searchText.trim() === '') return;
+
     dispatch({ type: 'SET-SEARCH-TEXT', payload: searchText });
 
-    const multi = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchText}`;
+    const URL_MULTI = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchText}`;
 
-    const getResults = () => {
-      let personResults = [];
+    const res = await fetch(URL_MULTI);
+    const { results } = await res.json();
+    if (results.length === 0) return;
 
-      fetch(multi)
-        .then((data) => data.json())
-        .then(({ results }) => {
-          console.log(results);
-          if (results.length === 0) return;
+    const personExactMatch = results.find((entry) => {
+      return (
+        entry.media_type === 'person' &&
+        entry.name.toLowerCase() === searchText.toLowerCase()
+      );
+    });
 
-          const finder = results.find((entry) => {
-            return (
-              entry.name?.toLowerCase() === searchText.toLowerCase() &&
-              entry.media_type === 'person'
-            );
-          });
+    // User searching for person
+    if (personExactMatch || results[0].media_type === 'person') {
+      const personID = (personExactMatch ? personExactMatch : results[0]).id;
 
-          console.log(finder && 'it is true');
+      const URL_ID = `https://api.themoviedb.org/3/person/${personID}/combined_credits?api_key=${process.env.REACT_APP_API_KEY}`;
 
-          if (results[0].media_type === 'person' || finder) {
-            // PERSON
-            let personID;
-
-            if (finder) {
-              personID = finder.id;
-            } else {
-              personID = results[0].id;
-            }
-
-            console.log(personID);
-
-            fetch(
-              `https://api.themoviedb.org/3/person/${personID}/combined_credits?api_key=${process.env.REACT_APP_API_KEY}`
-            )
-              .then((data) => data.json())
-              .then((stuff) => {
-                personResults = stuff.cast;
-                console.log(personResults);
-                dispatch({ type: 'SET-RESULTS', payload: personResults });
-                stuff.cast.forEach((entry) => {
-                  console.log(entry.media_type);
-                });
-              });
-          } else if (results[0].media_type !== 'person') {
-            // TV & MOVIE
-            const onlyTvAndMovie = results.filter(
-              (entry) => entry.media_type !== 'person'
-            );
-            dispatch({ type: 'SET-RESULTS', payload: onlyTvAndMovie });
-          }
-        });
-    };
-
-    getResults();
+      const res = await fetch(URL_ID);
+      const { cast: personResults } = await res.json();
+      console.log(personResults);
+      dispatch({ type: 'SET-RESULTS', payload: personResults });
+    }
+    // User searching for TV Show/Movie
+    else {
+      const tvAndMovieResults = results.filter(
+        (entry) => entry.media_type !== 'person'
+      );
+      dispatch({ type: 'SET-RESULTS', payload: tvAndMovieResults });
+    }
 
     navigate('/search');
   };
