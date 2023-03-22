8ae171a9ee390b7f0957515e59e60af855f05bb8
mehdi-sajedi <mehdiisajedi@gmail.com>
2023-03-22 16:41:08 -0700

    Minor style changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
-import movieReducer from "../features/movies/movieSlice";
-import showReducer from "../features/shows/showSlice";
-import personReducer from "../features/person/personSlice";
-import searchReducer from "../features/search/searchSlice";
-import userReducer from "../features/user/userSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import movieReducer from '../features/movies/movieSlice';
+import showReducer from '../features/shows/showSlice';
+import personReducer from '../features/person/personSlice';
+import searchReducer from '../features/search/searchSlice';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
